290c84b1d04fc17388c53bf3667f6b831b1f1078
sekercih <hkkskrc@gmail.com>
2021-08-12 22:35:54 +0300

    interface

--- src/generics/G06IkiParametreliGenericSinif.java
+++ src/generics/G06IkiParametreliGenericSinif.java
@@ -1,13 +1,27 @@
 package generics;
 
-public class G06IkiParametreliGenericSinif     {
+public class G06IkiParametreliGenericSinif {
+    public static void main(String[] args) {
+        Kisi<Integer, String> k1 = new Kisi<>(101, "Ahmet Yilmaz");
+        Kisi<Long, String> k2 = new Kisi<>(102L, "Mustafa Can");
 
+        //Comple-time hatasi
+//        Kisi<Boolean, String> k3 = new Kisi<>(true, "Hakki Can");
+
+        // Erisim
+        System.out.println(k1.getKey());
+        System.out.println(k2.getValue());
+    }
 }
 
 // K: Key
 // V: Value
 // 2 parametreli Generic class oluşuturulmasi
-class Kisi<K,V> {
+// Parametreler istenirse kisitlanabilir. Bunun icin ilgili veri tipinde extends edilebilir.
+// Bu isleme Parameter bounding denilmektedir.
+// Bu druumda yanlış veritipi girildiginde Comple-time hatasi alinir.
+class Kisi<K extends Number,V extends String> {
+    //    class Kisi<K,V> {
     private K key;
     private V value;
 
